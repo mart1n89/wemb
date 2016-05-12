01887e6404e2b0cfecc042b796a57dfb927b4310
mart1n89 <martin.kleinod@gmx.de>
2016-05-12 14:19:55 +0200

    Merged....

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-    echo "Hello World";
-    echo "Mkoaskdas";
-    echo "Test";
-    
-    echo "123";
-    
-    echo "Tecxld##gldsfkg";
-    echo 'i';
-    echo "123Test";
-    
-    echo "asdasdsa";
-    
-  
+    echo "Hello World"; 
 ?>
