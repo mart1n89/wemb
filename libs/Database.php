4e5af9f32de4c4784ad3a48f02bdee42b974fc74
accryn <moritzheeg@outlook.de>
2016-05-18 15:27:04 +0200

    sadfasdffsd

--- libs/Database.php
+++ libs/Database.php
@@ -4,6 +4,6 @@ class Database extends PDO
 {
     public function __construct() {
         //DB access parameters
-        //parent::__construct('mysql:host=http://projekt.wi.fh-flensburg.de;dbname=projekt2015b', 'projekt2015b', 'P2016b4');
+        parent::__construct('mysql:host=http://projekt.wi.fh-flensburg.de;dbname=projekt2015b', 'projekt2015b', 'P2016b4');
     }
 }
--- models/login_model.php
+++ models/login_model.php
@@ -3,16 +3,17 @@
 class Login_Model extends Model {
     
     public function __construct() {
-        
+        parent::__construct();
+        //echo md5('martin');
     }
     
     public function run(){
-//        $this->db->prepare("SELECT id FROM users WHERE login = :login AND password = :password");
-//        $this->db->excecute(array(':login' => $_POST['login'],
-//                                  ':password' => $_POST['password']
-//        ));
-//        
-//        $data = fetchAll();
+        $sth = $this->db->prepare("SELECT id FROM users WHERE login = :login AND password = :password");
+        $sth->excecute(array(':login' => $_POST['login'],
+                                  ':password' => $_POST['password']
+        ));
         
+        $data = $sth->fetchAll();
+        print_r($data);
     }
 }
