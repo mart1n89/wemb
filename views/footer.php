101ca3475944034014a8e733a83e20c58dcee92c
Jan009 <jan.conrad@stud.fh-flensburg.de>
2016-05-26 12:49:05 +0200

    footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,9 @@
 </div>
 <div id="footer"> 
-    (C) Footer
+    <label for="Impressum" style="color: silver" >Impressum</label></br>
+    <label  for="Impressum" style="color: silver" >about us</label>
+    <hr style="border: solid silver 1px"/>
+    <p>Copyright (C) 2016 - Alle Rechte vorbehalten</p>
 </div>  
 </body>
 </html>
