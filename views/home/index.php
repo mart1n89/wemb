1a1dbef2089d6176d99626c6a729db64070a5533
mart1n89 <martin.kleinod@gmx.de>
2016-06-02 14:54:26 +0200

    modified some redirecting issues

--- views/home/index.php
+++ views/home/index.php
@@ -1,5 +1,5 @@
 <h1>Willkommen beim Testimeter!</h1>
-<form method="post" action="<?php echo URL; ?>vote/index">
+<form method="post" action="<?php echo URL; ?>vote">
     <label>Code</label><input type="text" name="codeNo" />
     <input type="submit" value="aufrufen"/></br>
 </form>
