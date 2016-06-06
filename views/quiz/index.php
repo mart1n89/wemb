7a0b023b6f4193531fe0037f431ef3842b71363c
danie <danie@193.175.177.39>
2016-06-06 21:23:50 +0200

    Layoutanpassung

--- views/quiz/index.php
+++ views/quiz/index.php
@@ -1,16 +1,17 @@
 <h1>Quizübersicht</h1>
 <table>
-    <?php 
+    <?php
         foreach ($this->topicList as $key => $value){
-            echo '<tr>';
-            echo '<td>'.$value['topicName'].'</td>';
-            echo '<td><a href="#">Start</a></td>';
-            echo '<td><a href="'.URL.'quiz/edit/'.$value['topicID'].'">Edit</a></td>';
-            echo '<td><a href="'.URL.'quiz/delete/'.$value['topicID'].'">Delete</a></td>';
-            echo '</tr>';
+           
+        echo '<tr>';
+        echo '<td>'.$value['topicName'].'</td>';
+        echo '<td><a class="buttonCont" href="#">Start</a></td>';
+        echo '<td><a class="buttonCont" href="'.URL.'quiz/edit/'.$value['topicID'].'">Edit</a></td>';
+        echo '<td><a class="buttonCont" href="'.URL.'quiz/delete/'.$value['topicID'].'">Delete</a></td>';
+        echo '</tr>';            
         }
-        echo '<td><a href="'.URL.'quiz/create">Create</a></td>';
+        echo '<tr width="100%">';
+        echo '<td colspan="4" align="right"><a class="buttonCont" href="'.URL.'quiz/create">Create</a></td>';
         echo '</tr>';
     ?>
 </table>
-
