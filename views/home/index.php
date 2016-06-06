77bad007b93d06735dd2802535e299cde45d82e8
danie <danie@193.175.177.31>
2016-06-06 15:04:35 +0200

    Layoutanpassung

--- views/home/index.php
+++ views/home/index.php
@@ -1,8 +1,8 @@
 <h2>Willkommen beim Testimeter!</h2>
 <form method="post" action="<?php echo URL; ?>vote">
-    <table>
+    <table border="1">
         <tr>
-            <td><label>Code</label><input type="text" name="codeNo" /></td>
+            <td><label>Code</label><input type="text" name="codeNo"/></td>
             <td><input type="submit" value="aufrufen" class="buttonCont"/></br></td>
         </tr>
     </table>
