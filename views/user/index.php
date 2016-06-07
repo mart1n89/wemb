ed25d5897ef0ee71164fc392825a450b3222c65f
danie <danie@193.175.177.59>
2016-06-07 12:36:45 +0200

    Layoutanpassung

--- views/user/index.php
+++ views/user/index.php
@@ -1,26 +1,34 @@
-<h1>User</h1>
-<form method="post" action="<?php echo URL; ?>user/create">
-    <label>Username</label><input type="text" name="userName" /></br>
-    <label>Password</label><input type="text" name="password"/></br>
-    <label>Role</label>
-    <select name="role">
-        <option value="default">Default</option>
-        <option value="admin">Admin</option>
-    </select></br>
-    <label>&nbsp;</label><input type="submit" /></br>
-</form>
+<h2>User</h2>
 
-<table>
-<?php 
-    foreach ($this->userList as $key => $value){
-        echo '<tr>';
-        echo '<td>' . $value['userID'] . '</td>';
-        echo '<td>' . $value['userName'] . '</td>';
-        echo '<td>' . $value['role'] . '</td>';
-        echo '<td><a href="#">Edit</a></td>';
-        echo '<td><a href="#">Delete</a></td>';
-        echo '</tr>';
-    }
-?>
-
-</table>
+<div id="wrapper">
+    <div id="steuerung">
+	<table border="1" width="100%">    
+                <form method="post" action="<?php echo URL; ?>user/create">
+                    
+                    <tr><td><label>Username</label><input type="text" name="userName"/></br></td>
+                    <tr><td><label>Password</label><input type="text" name="password"/></br></td>
+                    <tr><td><label>Role</label>
+                        <select name="role">
+                            <option value="default">Default</option>
+                            <option value="admin">Admin</option>
+                            </select></br></td></tr>
+                     <tr><td align="right"><input type="submit" value="Speichern" class="buttonCont"/></br></td>
+                </form>
+            </table>
+    </div>
+    <div id="zweite_spalte">
+	<table border="1">
+                <?php 
+                    foreach ($this->userList as $key => $value){
+                        echo '<tr>';
+                        echo '<td>' . $value['userID'] . '</td>';
+                        echo '<td>' . $value['userName'] . '</td>';
+                        echo '<td>' . $value['role'] . '</td>';
+                        echo '<td><a href="#" class="buttonCont">Edit</a></td>';
+                        echo '<td><a href="#" class="buttonCont">Delete</a></td>';
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+    </div>
+</div>
