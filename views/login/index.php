df1e4add11d299fb4fd9cfb1d2e9e9410ff3a9d8
Jan009 <jan.conrad@stud.fh-flensburg.de>
2016-06-09 15:22:29 +0200

    Login

--- views/login/index.php
+++ views/login/index.php
@@ -1,7 +1,7 @@
 <h2>Login</h2>
 
 <form action="login/run" method="post">
-    <table border="1">
+    <table>
         <tr>
             <td><label>Username</label><input type="text" name="login"/></br</td>
         </tr>
@@ -9,13 +9,10 @@
             <td><label>Password</label><input type="password" name="password"/></br></td>
         </tr>
         <tr>
-            <td><label></label><input type="submit" value="Anmmelden" class="buttonContBig"/></br></td>
-        </tr>
-        <tr>
-            <td><label></label><input type="submit" value="Registrieren" class="buttonContBig"/></br></td>
+            <td><label></label><input type="submit" value="Anmmelden" class="buttonContBig" style="margin-right: 1vw;"/><input type="submit" value="Registrieren" class="buttonContBig"/></br></td>
         </tr>
         <tr >
-            <td><label></label><input type="submit" value="Passwort vergessen" class="submitClass" style="text-align:right;"/></br></td>
+            <td><input type="submit" value="Passwort vergessen" class="submitClass" style="margin-left: 5vw;"/></br></td>
         </tr>
     </table>   
 </form>
