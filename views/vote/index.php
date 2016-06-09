4d4446268b5783379d864de708f3c8c91b23cde0
mart1n89 <martin.kleinod@gmx.de>
2016-06-09 16:20:13 +0200

    writeBackFunction Vote

--- views/vote/index.php
+++ views/vote/index.php
@@ -6,23 +6,22 @@
         }
         else {
             
-            //Collect All Data
-            
             $this->controller->requireTopic($session['topicID']);
             $topic = $this->topic;
             
             $this->controller->requireQuestionSet($session['topicID']);
             $questions = $this->questions;
             
-            foreach ($questions as $key => $value) {
-                $questionIDs[$key] = $value['questionID'];
-            }
+                foreach ($questions as $key => $value) {
+                    $questionIDs[$key] = $value['questionID'];
+                }
             
             $this->controller->requireAnswers($questionIDs);
             $answers = $this->answers;
             
             echo "<h2>Thema: " . $topic['topicName'] . "<br />" ." Start: " . $session['start'] . "</h2>";
             echo "<form method=\"post\" action=\"" . URL  . "vote/send\">";
+            echo "<input type=\"hidden\" name=\"sessionID\" value=\"" . $session['sessionID'] ."\">";
             
             foreach ($questions as $key => $value){
                 echo "<div id =\"question_" . $key ."\">";
@@ -30,10 +29,10 @@
                 echo "<table>";
                 
                     foreach ($answers as $innerKey => $innerValue) {
-                        if ($value['questionID'] === $innerValue['questionID']) {
+                        if ($value['questionID'] == $innerValue['questionID']) {
                             echo "<tr>";
-                                echo "<td>" . $innerValue['answerText'] . "</td>";
-                                echo "<td>" . "<input type=\"radio\" name=\"question_" . $key ."\"/>" . "</td>";
+                                echo "<td>" . utf8_encode($innerValue['answerText']) . "</td>";
+                                echo "<td>" . "<input type=\"radio\" value=\"" . $innerValue['answerID'] .  "\" name=\"" . $innerValue['questionID'] . "\"/>" . "</td>";
                             echo "</tr>";   
                         }
                     }               
@@ -41,6 +40,7 @@
                 echo "</table>";
                 echo "</div>";
             }
+            
             echo "<br/><input type=\"submit\" value=\"Absenden\" class=\"buttonCont\"/>";
             echo "</form>";
         }       
