bf3fbe872f345f82a7ba7172550537c9116dc970
Jan009 <jan.conrad@stud.fh-flensburg.de>
2016-06-09 15:43:59 +0200

    Login

--- views/home/index.php
+++ views/home/index.php
@@ -1,14 +1,14 @@
 <h2>Willkommen beim Testimeter!</h2>
 <form method="post" action="<?php echo URL; ?>vote">
-<!--    <table>
+   <table>
         <tr>
             <td><label>Code</label><input type="text" name="codeNo"/></td>
-            <td valign="middle"><input type="submit" value="aufrufen" class="buttonCont"/></br></td>
+            <td valign="middle"><input type="submit" value="aufrufen" class="buttonContBig"/></br></td>
         </tr>
-    </table>-->
+    </table>
     
-    <label>Code</label><input type="text" name="codeNo"/>
-    <input type="submit" value="aufrufen" class="buttonContBig"/></br>
+<!--    <label>Code</label><input type="text" name="codeNo"/>
+    <input type="submit" value="aufrufen" class="buttonContBig"/></br>-->
 
 </form>
 
--- views/login/index.php
+++ views/login/index.php
@@ -1,7 +1,7 @@
 <h2>Login</h2>
 
 <form action="login/run" method="post">
-    <table>
+    <table style="vertical-align: middle;" border="1">
         <tr>
             <td><label>Username</label><input type="text" name="login"/></br</td>
         </tr>
@@ -9,10 +9,10 @@
             <td><label>Password</label><input type="password" name="password"/></br></td>
         </tr>
         <tr>
-            <td><label></label><input type="submit" value="Anmmelden" class="buttonContBig" style="margin-right: 1vw;"/><input type="submit" value="Registrieren" class="buttonContBig"/></br></td>
+            <td><label></label><input type="submit" value="Anmmelden" class="buttonContBig" style="margin-right:1vw;"/><input type="submit" value="Registrieren" class="buttonContBig"/></br></td>
         </tr>
         <tr >
-            <td><input type="submit" value="Passwort vergessen" class="submitClass" style="margin-left: 5vw;"/></br></td>
+            <td><input type="submit" value="Passwort vergessen" class="submitClass" style="margin-left: 6em;"/></br></td>
         </tr>
     </table>   
 </form>
