89192d54f6e4904ae777c29696dc29bdf2b5cc22
mart1n89 <martin.kleinod@gmx.de>
2016-06-09 13:34:21 +0200

    asdasdda

--- views/vote/index.php
+++ views/vote/index.php
@@ -1,10 +1,6 @@
 <?php
     $session = $this->session;
     
-//    echo '<pre>';
-//    print_r($session);
-//    echo '</pre>';
-    
         if ($session['isActive'] == 0) {
             echo "Tut uns Leid: Die Sitzung ist nicht aktiv!";
         }
@@ -25,21 +21,28 @@
             $this->controller->requireAnswers($questionIDs);
             $answers = $this->answers;
             
-            echo "<pre>";
-            print_r($answers);
-            echo "</pre>";
-            
             echo "<h2>Thema: " . $topic['topicName'] . "<br />" ." Start: " . $session['start'] . "</h2>";
             echo "<form method=\"post\" action=\"<?php echo URL; ?>vote/send\">";
             
             foreach ($questions as $key => $value){
                 echo "<div id =\"question_" . $key ."\">";
                 echo "<h3>" . utf8_encode($value['questionText']) . "</h3>";
+                echo "<table>";
+                
+                    foreach ($answers as $innerKey => $innerValue) {
+                        if ($value['questionID'] === $innerValue['questionID']) {
+                            echo "<tr>";
+                                echo "<td>" . $innerValue['answerText'] . "</td>";
+                                echo "<td>" . "<input type=\"radio\" name=\"question_" . $key ."\"/>" . "</td>";
+                            echo "</tr>";   
+                        }
+                    }               
+                
+                echo "</table>";
                 echo "</div>";
             }
             
             echo "</form>";
-
         }       
 ?>
 
