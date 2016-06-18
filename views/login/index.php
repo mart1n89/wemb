b0d088aff0930d1acd1c35f815ecbb9c8d017031
danie <danie@193.175.177.46>
2016-06-18 08:27:50 +0200

    Code Aufgeräumt

--- views/login/index.php
+++ views/login/index.php
@@ -1,18 +1,9 @@
 <h2>Login</h2>
-
 <form action="login/run" method="post">
     <table style="vertical-align: middle;" border="1">
-        <tr>
-            <td><label>Username</label><input type="text" name="login"/></br</td>
-        </tr>
-        <tr>
-            <td><label>Password</label><input type="password" name="password"/></br></td>
-        </tr>
-        <tr>
-            <td><label></label><input type="submit" value="Anmmelden" class="buttonContBig" style="margin-right:1vw;"/><input type="submit" value="Registrieren" class="buttonContBig"/></br></td>
-        </tr>
-        <tr >
-            <td><input type="submit" value="Passwort vergessen" class="submitClass" style="margin-left: 6em;"/></br></td>
-        </tr>
+        <tr><td><label>Username</label><input type="text" name="login"/></br</td></tr>
+        <tr><td><label>Password</label><input type="password" name="password"/></br></td></tr>
+        <tr><td><label></label><input type="submit" value="Anmmelden" class="buttonContBig" style="margin-right:1vw;"/><input type="submit" value="Registrieren" class="buttonContBig"/></br></td></tr>
+        <tr><td><input type="submit" value="Passwort vergessen" class="submitClass" style="margin-left: 6em;" /></br></td></tr>
     </table>   
 </form>
