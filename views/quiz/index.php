daa533f0c988b32ef813f12a23d9d8e9b5536834
danie <danie@193.175.177.46>
2016-06-18 08:21:10 +0200

    Quizübersicht Tabellenlayout angepasst. Create-Button Positioniert. Headlines Eingefügt.

--- views/quiz/index.php
+++ views/quiz/index.php
@@ -1,18 +1,23 @@
 <h1>Quiz&uuml;bersicht</h1>
-<table border="0" style="padding-left:5vw;">
     <?php
-        foreach ($this->topicList as $key => $value){
-           
-        echo '<tr>';
-        echo '<td class="tdConf">'.$value['topicName'].'</td>';
-        echo '<td class="tdConf"><a class="buttonPlay" href="#">Start<input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a></td>';
-        echo '<td class="tdConf"><a class="buttonPlay" href="'.URL.'quiz/edit/'.$value['topicID'].'">Edit<input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a></td>';
-        echo '<td class="tdConf"><a class="buttonPlay" href="'.URL.'quiz/delete/'.$value['topicID'].'"><input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a>
-            </td>';
-        echo '</tr>';            
-        }
+        echo '<table border="0" class="tdConf">';
+        echo '<thead>';
+        echo '<th>Thema</th>';
+        echo '<th>Starten</th>';
+        echo '<th>Bearbeiten</th>';
+        echo '<th>Löschen</th>';
+        echo '</thead>';
+            foreach ($this->topicList as $key => $value)
+                {           
+                    echo '<tr>';
+                    echo '<td >'.$value['topicName'].'</td>';
+                    echo '<td ><a class="buttonPlay" href="#"><input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a></td>';
+                    echo '<td ><a class="buttonPlay" href="'.URL.'quiz/edit/'.$value['topicID'].'"><input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a></td>';
+                    echo '<td ><a class="buttonPlay" href="'.URL.'quiz/delete/'.$value['topicID'].'"><input type="image" src="public/css/images/play.png" height="25vh" width="auto"></a></td>';
+                    echo '</tr>';
+                }
+        echo '</table>';        
         echo '<tr width="100%">';
-        echo '<td colspan="4" align="right" style="padding-top:2vh;"><a class="buttonContBig" href="'.URL.'quiz/create">Create</a></td>';
+        echo '<a class="buttonContBig" style="margin-left:4.5vw; margin-top:1vh;" href="'.URL.'quiz/create">Create</a>';
         echo '</tr>';
     ?>
-</table>
