581c2291bbc175dcfc57dc6d698c6c7aadb4616e
danie <danie@193.175.177.46>
2016-06-18 07:22:20 +0200

    veränderung an <hr> -> CSS

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 <div id="footer"> 
     <label for="Impressum" style="color: silver"><h4>Headcrash & Co</h4></label>
     <label for="Impressum" style="color: silver"> Christoph P., Daniel S.,Felix R., Jan C., Martin K., Moritz H. </label>
-    <hr style="border: solid silver 1px"/>
+    <hr/>
     <p><h6>Copyright (C) 2016 - Alle Rechte vorbehalten</h6></p>
 </div>  
 </body>
