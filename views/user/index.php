3da23c74b04858e2468d184a19b3df66f405779b
danie <danie@193.175.177.46>
2016-06-18 07:21:15 +0200

    Neuaufbaue des Userbereichs. Unterteilung in Anlegen und Bearbeiten. Tabellenanpassung

--- views/user/index.php
+++ views/user/index.php
@@ -1,39 +1,35 @@
-<!--<h2>Test</h2>-->
-<div id="wrapper">
-    <tr>
-        <td><h2>Benutzer bearbeiten</h2></td>
-        <td><h2>Benutzer bearbeiten</h2></td>
-    </tr> 
-    <div id="spalte_eins">
-        
-        <form method="post" action="<?php echo URL; ?>user/create">
-                <table border="1">             
-                    <tr><td><label>Username</label><input type="text" name="userName"/></br></td>
-                    <tr><td><label>Password</label><input type="text" name="password"/></br></td>
-                    <tr><td><label>Role</label>
-                         <select font-size="2em" name="role">
-                         <option value="default">Default</option>
-                         <option value="admin">Admin</option>
-                             </select></br></td></tr>
-                    <tr><td align="right"><input type="submit" value="Speichern" class="buttonContBig"/></br></td>
-                </table>
-            </form>
-    </div>
-    
-    <div id="spalte_zwei">
-        
-        <?php
-            echo '<table content-align="right" border="1" class="tableConf">';
-                foreach ($this->userList as $key => $value){
-                    echo '<tr>';
-                    echo '<td font-size="2em">' . $value['userID'] . '</td>';
-                    echo '<td>' . $value['userName'] . '</td>';
-                    echo '<td>' . $value['role'] . '</td>';
-                    echo '<td><a href="#" class="buttonContSmall">Edit</a></td>';
-                    echo '<td><a href="#" class="buttonContSmall">Delete</a></td>';
-                    echo '</tr>';
-                }
-                echo '</table>';
-            ?>
-    </div>
+<div>
+    <h2>Benutzer anlegen</h2>
+    <form method="post" action="<?php echo URL; ?>user/create">
+            <table border="0">             
+                <tr><td><label>Username</label><input type="text" name="userName"/></br></td>
+                <tr><td><label>Password</label><input type="text" name="password"/></br></td>
+                <tr><td><label>Role</label>
+                     <select font-size="2em" name="role">
+                     <option value="default">Default</option>
+                     <option value="admin">Admin</option>
+                         </select></br></td></tr>
+                <tr><td><label></label><input type="submit" value="Speichern" class="buttonContBig"/></br></td>
+            </table>
+        </form>
+</div>
+</br>
+
+ <hr/>   
+ 
+<div>
+    <h2>Benutzer bearbeiten</h2>
+    <?php
+        echo '<table class="tdConf">';
+            foreach ($this->userList as $key => $value){
+                echo '<tr>';
+                echo '<td>' . $value['userID'] . '</td>';
+                echo '<td>' . $value['userName'] . '</td>';
+                echo '<td>' . $value['role'] . '</td>';
+                echo '<td><a href="#" class="buttonContSmall">Edit</a></td>';
+                echo '<td><a href="#" class="buttonContSmall">Delete</a></td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        ?>
 </div>
