66acaedb1fe0a0f2966f5ef5f17c0e0894544ac6
mart1n89 <martin.kleinod@gmx.de>
2016-06-23 16:01:00 +0200

    added registration

--- controllers/login.php
+++ controllers/login.php
@@ -10,6 +10,20 @@ class Login extends Controller {
     }
     
     function run() {
-        $this->model->run();
+        //Array ( [login] => asd [password] => asd [loginBtn] => Anmelden ) 
+        if (null != (filter_input(INPUT_POST, 'loginBtn'))) {
+            $this->model->run();
+        }
+        if (null != (filter_input(INPUT_POST, 'registerBtn'))) {
+            $this->view->render('login/register');
+        }
+        if (null != (filter_input(INPUT_POST, 'nopassBtn'))) {
+            echo "Do lost pass";
+        }        
+    }
+    
+    function register() {
+        //@ToDo: Email schicken.
+        print_r(filter_input_array(INPUT_POST));
     }
 }
