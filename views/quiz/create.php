925607ee4176e52864346b2aedec8d6857bd5030
danie <danie@172.16.86.78>
2016-06-23 15:26:12 +0200

    DIV Inhalt Eingerückt

--- views/quiz/create.php
+++ views/quiz/create.php
@@ -1,9 +1,9 @@
 <h1>Quiz anlegen</h1>
 <body onload="init()">
-<div id="area">
+<div id="area" style="margin-left:4.5vw">
     
 </div>
-    <div id="save">
+    <div id="save" style="margin-left:4.5vw">
         
     </div>
 </body>
