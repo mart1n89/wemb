195d78150fd54e6eb02f08b723dfa53ae67b5ef4
accryn <moritzheeg@outlook.de>
2016-06-24 17:20:48 +0200

    adfasdfsdfsdf

--- views/result/show.php
+++ views/result/show.php
@@ -1,2 +1,9 @@
 <?php
-print_r($this->results);
+$questionID = 0;
+foreach ($this->results as $key => $value) {
+    if ($value['questionID'] !== $questionID) {
+        $questionID = $value['questionID'];
+        echo 'Frage: ' . $value['questionText'].'</br>';
+    }
+    echo $value['answerText']. ' ' . $value['clicks'].'</br>';
+}
