b13e65d423cf5e3cc74bc6e48957a0d3ee4388aa
danie <danie@193.175.177.45>
2016-06-24 19:40:38 +0200

    About und FAQ Implementiert

--- views/help/index.php
+++ views/help/index.php
@@ -1,1 +1,21 @@
-<h1>Help</h1>
+<h2>Häufig gestellte Fragen</h2>
+<div style="text-align: justify; margin-left:4.5vw; width:40vw;" >
+    
+    <p><b>Für wen ist Testimeter nutzbar?</b></p>
+    <p>Testimeter ist für alle Angehörigen der Hochschule Flensburg nach Beantragung eines Nutzeraccounts beim Administrator (aktuell Herr Prof. Müller) zugänglich.</p>
+    <p><b>Wie bekomme ich einen Account?</b></p>
+    <p>Die Beantragung kann formlos per Mail an mueller@hs-flensburg.de erfolgen. Studierende fügen dem Antrag bitte eine Immatrikulationsbescheinigung bei.</p>
+    <p><b>Was bringt Testimeter der Hochschule Flensburg?</b></p>
+    <p>
+       Nutzen für die Hochschule besteht in der Möglichkeit, den Lehrenden aufzeigen zu können, ob die vermittelten Inhalte korrekt von den Studierenden 
+       verstanden wurden und diese in der Lage sind, entsprechende Fragen zum Themengebiet zu beantworten. Aus der Beantwortung der Fragen können die Lehrenden 
+       erkennen, ob andere Herangehensweisen oder auch ggfs. vertiefende Informationen zu Darlegung der vermittelten Sachverhalte notwendig erscheinen.
+    </p>
+    <p><b>Wie konfiguriere ich den Timer richtig?</b></p>
+    <p>
+        Jeder Nutzer hat einen individuell konfigurierbaren „User-Timer“. Zusätzlich besteht die Möglichkeit, pro Fragerunde einen sog. „Session-Timer“ zu setzen, 
+        der nur für die Fragerunde besteht. Sollten Sie keinen „Session-Timer“ vergeben, so wählt das System Ihren „User-Timer“ für die Fragerunde aus.
+    </p>
+    <p><b>Wie ist Testimeter erreichbar?</b></p>
+    <p>Testimeter ist im WLAN „eduroam“ der Hochschule Flensburg und außerhalb der Hochschule über VPN zugänglich. Ein externer Zugriff ist nicht möglich.</p>
+</div>
