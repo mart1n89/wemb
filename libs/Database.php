9daca67de2100f12cf9d4eadca0520c93a52b9f3
mart1n89 <martin.kleinod@gmx.de>
2016-06-26 10:42:09 +0200

    again db error

--- libs/Database.php
+++ libs/Database.php
@@ -7,6 +7,7 @@ class DB extends PDO
     public function __construct() {
         try {
             parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+            Session::set('dbError', null);
         } catch (Exception $ex) {
             Session::set('dbError', $ex->getMessage());
         }        
