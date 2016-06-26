ce0f90c5cb6b484d2d8a7f96a1b68cad25faaff7
mart1n89 <martin.kleinod@gmx.de>
2016-06-26 10:39:27 +0200

    added Errorhandling for db connection

--- libs/Database.php
+++ libs/Database.php
@@ -1,8 +1,14 @@
 <?php
 
+error_reporting(0);
+
 class DB extends PDO
 {
     public function __construct() {
-        parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+        try {
+            parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+        } catch (Exception $ex) {
+            Session::set('dbError', $ex->getMessage());
+        }        
     }
 }
