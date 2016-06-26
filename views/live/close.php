e4f0df6cf733792a317d0838e2fcab6bd6e209ff
accryn <moritzheeg@outlook.de>
2016-06-26 12:25:44 +0200

    quiz anlegen angepasst

--- views/live/close.php
+++ views/live/close.php
@@ -1,4 +1,5 @@
 <?php 
+    echo '<h1>Quiz beendet</h1>';
     echo 'Ihr Quiz wurde geschlossen!</br>'; 
     echo 'Sehen sie sich das Ergebnis '. '<a href="../../result/show/'. $this->code .'">hier</a>' . ' an.' 
 ?>
--- views/quiz/create.php
+++ views/quiz/create.php
@@ -1,6 +1,6 @@
 <h2>Quiz anlegen</h2>
-<p>Legen Sie hier ihr Quiz an. Stellen Sie beliebig viele Fragen zu einem vermittelten Thema und markieren mit dem Radio-Button die richtige Antwort.</p></br>
- 
+<p>Legen Sie hier ihr Quiz an. Stellen Sie beliebig viele Fragen zu einem vermittelten Thema und markieren mit dem Radio-Button die richtige Antwort.</p>
+<p>Sonderzeichen wie "[", ";" oder "<" sind nicht erlaubt. </p></br>
 <body onload="init()">
 <div id="area" style="margin-left:4.5vw"> </div>
 <div id="save" style="margin-left:4.5vw; margin-top:2vh"> </div>
