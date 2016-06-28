93ecdd12f2c4f09333f488c2b3f86360668fd55a
danie <danie@172.16.84.41>
2016-06-28 14:32:44 +0200

    about und help schriftgrad anpassen

--- views/about/index.php
+++ views/about/index.php
@@ -1,5 +1,5 @@
 <h2>&Uuml;ber Testimeter</h2>
-<div id="textBlock">
+<div id="textBlock" style="font-size: 1.2em;">
     <p>Testimeter bietet Ihnen die M&ouml;glichkeit, einer Zielgruppe Fragen zu stellen, die diese dann &uuml;ber ein Ger&auml;t mit Internetzugang anonym beantworten kann. Die Antwortm&ouml;glichkeiten pflegen Sie bereits vorher ein und hinterlegen, welche korrekt ist bzw. sind (mehrere L&ouml;sungen sind m&ouml;glich).</p> 
     <p>Sie selbst bestimmen &uuml;ber einen Timer, wie lange die Zielgruppe Zeit haben soll, um die Fragen zu beantworten.</p>
     <p>&Uuml;ber einen generierten Code erh&auml;lt die Zielgruppe Zugang zu Ihrer Fragerunde. In Echtzeit werden anschliessend die Ergebnisse der Fragerunde f&uuml;r Sie visualisiert. Das Endergebnis k&ouml;nnen Sie der Zielgruppe abschliessend pr&auml;sentieren, wenn Sie m&ouml;chten.</p>
